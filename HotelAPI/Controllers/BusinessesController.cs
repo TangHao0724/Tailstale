71beafb23038e57733f0d0ffbbfcf8614d92e676
huang <owjiin@gmail.com>
2024-06-28 14:24:45 +0800

    AddHotelAPI

---
+++ HotelAPI/Controllers/BusinessesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelAPI.Models;
+using WebApplication1.DTO;
+
+namespace HotelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessesController : ControllerBase
+    {
+        public TailstaleContext _context;
+
+        public BusinessesController(TailstaleContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Businesses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Business>>> GetBusinesses()
+        {
+            return await _context.Businesses.ToListAsync();
+        }
+
+        // GET: api/Businesses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Business>> GetBusiness(int id)
+        {
+            var business = await _context.Businesses.FindAsync(id);
+
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            return business;
+        }
+
+        // PUT: api/Businesses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBusiness(int id, Business business)
+        {
+            if (id != business.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(business).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusinessExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Businesses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<string> PostBusiness(BusinessDTO businessDTO)
+        {
+            
+            Business business = new Business()
+            {
+                Name = businessDTO.Name,
+                Password = businessDTO.Password,
+                FkStatusId = businessDTO.status
+            };
+
+            _context.Businesses.Add(business);
+            await _context.SaveChangesAsync();
+
+            return "新增成功";
+        }
+
+        // DELETE: api/Businesses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBusiness(int id)
+        {
+            var business = await _context.Businesses.FindAsync(id);
+            if (business == null)
+            {
+                return NotFound();
+            }
+
+            _context.Businesses.Remove(business);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BusinessExists(int id)
+        {
+            return _context.Businesses.Any(e => e.Id == id);
+        }
+    }
+}
