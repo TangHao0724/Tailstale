19a0bc774d146fcff4c6417d638325ce8926e04d
TangHao <thomas35011@gmail.com>
2024-07-21 16:57:00 +0800

    製作登入分區

---
+++ Tailstale/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Tailstale.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.userID = HttpContext.Session.GetInt32("loginID");
+            ViewBag.userType = HttpContext.Session.GetInt32("loginType");
+
+            return View();
+        }
+    }
+
+}
---
+++ Tailstale/wwwroot/js/keeper/IndexVue.js
@@ -0,0 +1,6 @@
+﻿var App = Vue.createApp({
+
+
+})
+app.mount('#App');
+
