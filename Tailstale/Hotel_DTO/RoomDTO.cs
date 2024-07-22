645ae025d795865df61a55c234358b01b9d9a536
arueiruei <arueiruei520@gmail.com>
2024-07-23 00:09:27 +0800

    re

--- Tailstale/Hotel_DTO/RoomDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.DTO
-{
-    public class RoomDTO
-    {
-        public int RoomID { get; set; }
-        public int HotelId { get; set; }
-        public int Price { get; set; }
-    }
-}
